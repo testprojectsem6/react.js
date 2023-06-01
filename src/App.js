24826f3de7a5292580c430102d21b1f54b2a0b68
testprojectsem6 <134890636+testprojectsem6@users.noreply.github.com>
2023-06-01 15:07:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Amrooo
         </a>
       </header>
     </div>
